e5cc6ca4c28451ba2b7ccf55daa37d8ba3605868
Md Rashidul Azam (Nishan) <arosh019@gmail.com>
2024-04-27 12:23:46 +0600

    remove my previous text domain/prefix link

--- woomega.php
+++ woomega.php
@@ -8,7 +8,7 @@
  * Requires at least: 5.8
  * Requires PHP:      7.4
  * Author:            BDSOFTcr
- * Author URI:        https://www.bdsoftcreation.com/
+ * Author URI:        https://www.nishanmazumder.com/
  * License:           GPL v2 or later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:       nm_theme
